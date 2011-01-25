f75f3ed8440aac7266f8b1aed33ed8f91b62a466
Juliana Pena <limited.mage@gmail.com>
2011-01-24 23:49:00 -0600

    Pretty good push notifications implementation :)

--- Server/tile.php
+++ Server/tile.php
@@ -1,7 +1,9 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 $baseurl = "/home/julianapena/winmilk.julianapena.com";
-$imageuri = "/tiles/Tile0.png";
+/*$imageuri = "/tiles/Tile0.png";
 
 if (isset($_REQUEST["count"]))
 {
@@ -20,4 +22,60 @@ if (isset($_REQUEST["count"]))
 header("Content-type: image/png");
 $im = imagecreatefrompng($baseurl.$imageuri);
 imagepng($im);
+*/
+
+
+if (isset($_REQUEST["count"])) {
+	$number = (int) $_REQUEST["count"];
+}
+else {
+	$number = 0;
+}
+
+$width = 173;
+$height = 173;
+
+$image = imagecreatetruecolor($width, $height);
+imagealphablending($image,true);
+imagesavealpha($image,true);
+
+$trans = imagecolorallocatealpha($image, 0, 0, 0, 127);
+$white = imagecolorallocate($image, 0, 0, 255);
+$black = imagecolorallocate($image, 0, 0, 0);
+
+imagealphablending($image,false);
+imagefill($image, 0, 0, $trans);
+imagealphablending($image,true);
+
+$icon = imagecreatefrompng("$baseurl/numbers/icon.png");
+$w = imagesx($icon);
+$h = imagesy($icon);
+
+if ($number <= 0) {
+	imagecopy($image, $icon, 45, 40, 0, 0, $w, $h);
+}
+else if ($number < 10) {
+	imagecopy($image, $icon, 30, 40, 0, 0, $w, $h);
+	
+	$count = imagecreatefrompng("$baseurl/numbers/".$_REQUEST["count"].".png");
+	$w = imagesx($count);
+	$h = imagesy($count);
+	
+	imagecopy($image, $count, 95, 58, 0, 0, $w, $h);
+}
+else {
+	imagecopy($image, $icon, 20, 40, 0, 0, $w, $h);
+	
+	$count = imagecreatefrompng("$baseurl/numbers/9plus.png");
+	$w = imagesx($count);
+	$h = imagesy($count);
+	
+	imagecopy($image, $count, 85, 58, 0, 0, $w, $h);
+	
+	
+}
+
+header("Content-type: image/png");
+imagepng($image);
+
 ?>
